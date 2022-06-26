de0e3da604c194473d7e3c83aeb6222d89078c64
Jose Pablo Vega <josepablo.x@gmail.com>
2022-06-26 00:29:03 -0600

    cambio pequeño en consola admin

--- BloggerApp/models.py
+++ BloggerApp/models.py
@@ -1,7 +1,13 @@
+from operator import truediv
+from pyexpat import model
+from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User
 
 class Avatar(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="avatars", null=True, blank=True)
+        
+    def __str__(self):
+        return f'{self.user}'
 
